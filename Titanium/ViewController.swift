c185fb409ad3eff3ea6bd4e81596fb446ea4f6d8
Cl0ud7 <agilmi00@estudiantes.unileon.es>
2023-04-30 01:10:06 +0200

    Added - iOS support

--- Titanium/ViewController.swift
+++ Titanium/ViewController.swift
@@ -5,10 +5,16 @@
 //  Created by Cl0ud7.
 //
 
-import Cocoa
 import Metal
 import MetalKit
 
+#if os(macOS)
+import Cocoa
+#elseif os(iOS)
+import UIKit
+#endif
+
+#if os(macOS)
 class ViewController: NSViewController {
 
     @IBOutlet weak var metalView: MTKView!
@@ -29,4 +35,27 @@ class ViewController: NSViewController {
         }
     }
 }
+#elseif os(iOS)
+class ViewController: UIViewController {
+
+    @IBOutlet weak var metalView: MTKView!
+    @IBOutlet weak var GPULabel: UILabel!
+    var m_Renderer: Renderer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let Device = MTLCreateSystemDefaultDevice()!
+        GPULabel.text = Device.name + " [Metal]" 
+        m_Renderer = Renderer(device: Device, view: metalView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+#endif
 
